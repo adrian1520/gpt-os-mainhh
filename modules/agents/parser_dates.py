df7b0297f2f2a86056a673e7e09311c417ab2050
adrian1520 <adrian.adamowicz.x@gmail.com>
2026-03-23 18:00:57 +0100

    add global event id (hash)

--- modules/agents/parser_dates.py
+++ modules/agents/parser_dates.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import time
+import hashlib
 
 INPUT_PATH = "tasks/task.json"
 OUTPUT_DIR = "events"
@@ -13,9 +14,12 @@ with open(INPUT_PATH) as f:
 
 content = task.get("content","")
 
+EVENT_ID = hashlib.sha1(content.encode()).hexdigest()[:12]
+
 dates = re.findall(r'[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}', content)
 
 result = {
+    "id": EVENT_ID,
     "timestamp": int(time.time()),
     "type": "parser_dates",
     "dates": dates,
